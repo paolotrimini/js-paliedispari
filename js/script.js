bf30a05a88d90097a4295f1b152e5fb346f65269
paolotrimini <paolo.trimini@gmail.com>
2021-03-12 13:52:49 +0100

    caricamento file

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Esercizio 1 - Una funzione per capire se la parola è palindroma
+
+
+
+
+
+
+// Esercizio 2 - L'utente sceglie pari o dispari e un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer.
+// Sommiamo i 2 numeri e dichiariamo chi ha vinto.

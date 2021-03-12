c38b461dd3ef19fd1bdeb849be1704092ae7bcb1
paolotrimini <paolo.trimini@gmail.com>
2021-03-12 17:34:39 +0100

    fixing es pari-dispari

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // Esercizio 1 - Scrivere una funzione per capire se la parola è palindroma
 
-    var myWord = prompt('inserisci una parola: ');
-
-     // FUNZIONE - calcolo parola palindroma
-
-    function isPali(myStr) {
-        return myStr === myStr.split('').reverse().join('');
-    }
-
-    // la parola inserita è palindroma?
-
-    if (isPali(myWord)) {
-
-        console.log('la parola è palindroma');
-
-    } else {
-
-        console.log('la parola NON è palindroma');
-    }
+//var myWord = prompt('inserisci una parola: ');
+//
+// // FUNZIONE - calcolo parola palindroma
+//
+//function isPali(myStr) {
+//    return myStr === myStr.split('').reverse().join('');
+//}
+//
+//// la parola inserita è palindroma?
+//
+//if (isPali(myWord)) {
+//
+//    console.log('la parola è palindroma');
+//
+//} else {
+//
+//    console.log('la parola NON è palindroma');
+//}
 
 
 
@@ -39,21 +39,58 @@
 // Sommiamo i 2 numeri e dichiariamo chi ha vinto.
 
 
-// utente sceglie numero pari
+// FUNZIONE - genera numero random
 
-var userNumber = 3;
-var pcNumber = parseInt(prompt('inserire n° random del computer'));
-
-
-
-// creazione numero random
-
-function getRnd (min, max) {
+function computerNumber(min, max) {
     var minRnd = min;
     var maxRnd = max - minRnd + 1;
     var rnd1 = Math.floor(Math.random() * maxRnd) + minRnd;
     return rnd1;
 }
+//console.log(computerNumber(1, 5));
 
-console.log(getRnd(min, max));
 
+// FUNZIONE - è pari o dispari?
+
+function isPair() {
+    return finalUser % 2 === 0;
+}
+console.log(isPair());
+
+
+
+// scelta "numero" e "pari - dispari" User
+
+var userNumber = 2;
+var userChoice = true;
+
+
+var finalUser = userNumber + computerNumber(1,5);
+console.log('somma User: ' + finalUser);
+
+// numero Rnd Computer
+
+var finalComputer = computerNumber(1,5);
+console.log('numero rnd Computer: ' + finalComputer);
+
+
+// vince User o Computer?
+
+if ((finalUser > finalComputer) && userChoice === isPair(userChoice) ) {
+
+    console.log('vince USER con PARI: ' + finalUser);
+
+}else if ((finalUser > finalComputer) && userChoice !== isPair(userChoice) ) {
+
+    console.log('vince USER con DISPARI: ' + finalUser);
+
+}else if ((finalUser < finalComputer) && userChoice === isPair(userChoice) ) {
+
+    console.log('vince COMPUTER con PARI: ' + finalComputer);
+
+}else if ((finalUser < finalComputer) && userChoice !== isPair(userChoice) ) {
+
+    console.log('vince COMPUTER con DISPARI: ' + finalComputer);
+} else {
+    console.log('parità!')
+}
